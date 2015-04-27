6a066f5097882791fa72f2ed6a03d1e48c27b7f3
Ryan Chen <ryanchen@yahoo-inc.com>
2015-04-26 23:03:10 -0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var myName = 'Ryan Chen';
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Ryan', name: myName });
+});
+
+router.get('/me', function(req, res, next) {
+  res.render('me', {name: myName});
+});
+
+module.exports = router;
