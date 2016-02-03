c582621d4a4d4ef478277abef88ca6fbb5e632f3
Ryan Chen <ryanlc@ryanlc-macbookpro.roam.corp.google.com>
2016-02-02 23:22:48 -0800

    Modify configuration

--- ghost/config.js
+++ ghost/config.js
@@ -11,19 +11,19 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://www.ryanchen.com',
         mail: {},
         database: {
             client: 'sqlite3',
             connection: {
                 filename: path.join(__dirname, '/content/data/ghost.db')
             },
-            debug: false
+            debug: true
         },
 
         server: {
             host: '127.0.0.1',
-            port: '2368'
+            port: '80'
         }
     },
 
